377f607d1f55a5385e6c97704466b16fabd9e9bd
tkummermehr <30153917+tkummermehr@users.noreply.github.com>
2026-05-18 14:35:50 +0200

    Update initial value for LAN865x Rev.B1 (sync branch) (#83)

--- inc/tc6.h
+++ inc/tc6.h
@@ -54,8 +54,8 @@ extern "C" {
 
 #define TC6_LIB_VER_MAJOR  (3U)
 #define TC6_LIB_VER_MINOR  (1U)
-#define TC6_LIB_VER_BUGFIX (3U)
-#define TC6_LIB_VER_STRING "V3.1.3-sync"
+#define TC6_LIB_VER_BUGFIX (6U)
+#define TC6_LIB_VER_STRING "V3.1.6-sync"
 
 struct TC6_t;
 typedef struct TC6_t TC6_t;
